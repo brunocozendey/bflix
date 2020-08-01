a53e6c9056145f96ea576434411789bbfb4ffea1
brunocozendey <brunocozendey@gmail.com>
2020-08-01 07:47:01 -0300

    Inserido cadastro de video
    Signed-off-by: brunocozendey <brunocozendey@gmail.com>

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -2,15 +2,17 @@ import React from 'react';
 import Logo from '../../assets/imgs/logo.png';
 import './menu.css';
 import Button from '../Button';
+import { Link } from 'react-router-dom';
+
 
 function Menu() {
     return (
       <nav className="Menu">  
-          <a href="/">
+          <Link to="/">
           <img  className="Logo" src={ Logo } alt="Logo Bflix"/> 
-          </a>
+          </Link>
 
-          <Button as = "a" href="/" className="ButtonLink">
+          <Button as = { Link } to="/cadastro/video" className="ButtonLink">
               Novo Vídeo
           </Button>
       </nav>  
